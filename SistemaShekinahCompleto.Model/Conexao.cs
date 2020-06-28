be0e263cd7528391de1f09b6a3339828d814d53c
queiroz programador <queirozandrey5@gmail.com>
2020-06-28 10:12:04 -0400

    isto é uma correção na conexao

--- SistemaShekinahCompleto.Model/Conexao.cs
+++ SistemaShekinahCompleto.Model/Conexao.cs
@@ -12,20 +12,24 @@ namespace SistemaShekinahCompleto.Model
         Provedores prov;
         public string mensagens { get; private set; }
         string[] DataSources = {"Monica",@"PC-SERVIDOR,1433\SQLEXPRESS" };
-        
+        SqlConnection conata;
+        SqlConnection concaixa;
 
         public Conexao()
         {
             prov = Provedores.pc_servidor;
+            conata = new SqlConnection(@"Data Source = " + DataSources[(int)prov] + "; DataBase = BDAtaFinal; User ID = sa ; Password = yerdna");
+            concaixa = new SqlConnection(@"Data Source = "+DataSources[(int)prov]+"; DataBase = BDCaixa ; User ID = sa ; Password = yerdna");
+            
         }
         public SqlConnection NovaConexaoBdAtaFinal()
         {
 
-            return new SqlConnection(@"Data Source = "+DataSources[(int)prov] +"; DataBase = BDAtaFinal; User ID = sa ; Password = yerdna");
+            return conata;
         }
         public SqlConnection NovaConexaoBdCaixa()
         {
-            return new SqlConnection(@"Data Source = "+DataSources[(int)prov]+"; DataBase = BDCaixa ; User ID = sa ; Password = yerdna");
+            return concaixa;
         }
         void Messagens(object sender, SqlInfoMessageEventArgs e)
         {
